9cb7c1f18aaf27a1ffc61c216b2eefad7f7e04a1
Sravan Pathipati <sravan.pathipati@rackspace.com>
2018-11-29 20:52:21 -0600

    Subsription Manager: Adding base stuff

---
+++ popup.js
@@ -0,0 +1,9 @@
+// DOM Variables 
+
+let track = document.getElementById('track');
+let main = document.getElementById('main');
+
+track.addEventListener('click',function(){
+	main.innerText += 'Sucessfully added';
+
+});
